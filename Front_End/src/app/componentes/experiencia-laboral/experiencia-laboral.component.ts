5edb2f8dd4ea7ee7a3066ea6566339df7e14f755
Veronica Ludemann <veronicaludemann67@gmail.com>
2022-06-15 09:07:02 -0300

    servicios y model experiencia
    creacion y edicion de servicio, componente y modelo experieciencia

--- Front_End/src/app/componentes/experiencia-laboral/experiencia-laboral.component.ts
+++ Front_End/src/app/componentes/experiencia-laboral/experiencia-laboral.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Experiencia } from 'src/app/model/experiencia.model';
+import { ExperienciaService } from 'src/app/service/experiencia.service';
 
 @Component({
   selector: 'app-experiencia-laboral',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiencia-laboral.component.css']
 })
 export class ExperienciaLaboralComponent implements OnInit {
+  public experiencias: Experiencia[]=[];
 
-  constructor() { }
+  constructor(private experienciaService: ExperienciaService) { }
 
   ngOnInit(): void {
+    this.getExperiencias();
   }
 
+  public getExperiencias (): void{
+    this.experienciaService.getExperiencias().subscribe({
+      next:(Response:Experiencia[])=> {
+        this.experiencias= Response;
+      },
+      error:(error: HttpErrorResponse)=> {
+        alert(error.message);
+      }
+  });
+    }
 }
+
---
+++ Front_End/src/app/model/experiencia.model.ts
@@ -0,0 +1,8 @@
+export interface Experiencia {
+  id:number;
+  empresa:string;
+  cargo:string;
+  Fecha_ingreso:string;
+  Fecha_egreso:string
+
+}
