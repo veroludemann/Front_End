d8a1ff86fd4ad6a61fae7b03edd2f7873937af0c
Veronica Ludemann <veronicaludemann67@gmail.com>
2022-07-01 13:10:53 -0300

    creacion de modal educacion
    botones de edicion y modal

--- Front_End/src/app/componentes/educacion/educacion.component.ts
+++ Front_End/src/app/componentes/educacion/educacion.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Educacion } from 'src/app/model/educacion.model';
 import { EducacionService } from 'src/app/service/educacion.service';
 
@@ -10,6 +11,8 @@ import { EducacionService } from 'src/app/service/educacion.service';
 })
 export class EducacionComponent implements OnInit {
   public educaciones!: Educacion[];
+  public editEducacion!: Educacion;
+  public deleteEducacion!: Educacion;
 
 
   constructor(private educacionService: EducacionService) { }
@@ -27,6 +30,70 @@ export class EducacionComponent implements OnInit {
         alert(error.message);
       }
     });
+  }
+
+  public onAddEducacion(addForm: NgForm): void{
+    document.getElementById("add-educacion-form")!.click();
+    this.educacionService.addEducacion(addForm.value).subscribe({
+      next: (response: Educacion) => {
+        console.log(response);
+        this.getEducaciones();
+        addForm.reset();
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    });
+  }
+
+  public onUpdateEducacion(educacion: Educacion): void{
+    this.educacionService.editEducacion(educacion).subscribe({
+      next: (response: Educacion) => {
+        console.log(response);
+        this.getEducaciones();
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    });
+  }
+
+  public onDeleteEducacion(educacionId: number): void {
+    this.educacionService.deleteEducacion(educacionId).subscribe({
+      next: (response: void) => {
+        console.log(response);
+        this.getEducaciones();
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    });
+  }
+
+
+
+
+  public onOpenModal(educacion:Educacion, mode: string): void{
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add'){
+      button.setAttribute('data-target', '#addEducacionModal');
+    }
+    if (mode === 'edit'){
+      this.editEducacion = educacion;
+      button.setAttribute('data-target', '#updateEducacionModal');
+    }
+    if (mode === 'delete'){
+      this.deleteEducacion = educacion;
+      button.setAttribute('data-target', '#deleteEducacionModal');
+    }
+    container?.appendChild(button);
+    button.click();
+
   }
 
 
