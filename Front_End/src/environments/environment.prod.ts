a4bbbd6e07552cdbe6ee6ad92c0e7a9e5e28498d
Veronica Ludemann <veronicaludemann67@gmail.com>
2022-07-28 13:21:08 -0300

    deploy fire

--- Front_End/src/environments/environment.prod.ts
+++ Front_End/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl:'http://localhost:8080'
+  apiBaseUrl:'https://backend-argp.herokuapp.com/',
+   authURL:'https://backend-argp.herokuapp.com/auth'
 };
--- Front_End/src/environments/environment.ts
+++ Front_End/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-   apiBaseUrl:'http://localhost:8080'
+   apiBaseUrl:'https://backend-argp.herokuapp.com/',
+   authURL:'https://backend-argp.herokuapp.com/auth'
 };
 
 /*
